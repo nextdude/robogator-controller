fa6868429d8c54b65fbe2bbe92b7ea85bc648eac
Robert Lyons <nextdude@gmail.com>
2017-06-05 04:39:19 -0400

    updates

--- src/app.py
+++ src/app.py
@@ -17,15 +17,24 @@ app = Flask(__name__)
 sockets = Sockets(app)
     
 def handle_actions(req):
-    res = { "actions": req['actions'], "result": {"code": 0} }
+    res = { "actions": req['actions'], "result": {"code": 0, "values": []} }
     for action in req['actions']:
         method = action[0]
         args = action[1:]
         if method == 'set_led':
             BP.set_led(*args)
             time.sleep(0.01)
+        elif method == 'sleep':
+            time.sleep(*args)
         elif method == 'set_motor_power':
             BP.set_motor_power(*args)
+        elif method == 'get_sensor':
+            try:
+                value = BP.get_sensor(*args)
+            except brickpi3.SensorError as error:
+                print(error)
+                value = 255
+            res['result']['values'].append(value)
     return res
 
 @app.route("/")
@@ -43,7 +52,7 @@ def gator(ws):
 
 @app.route("/<section>")
 def show_section(section):
-    if section not in ['home', 'think', 'walk', 'attack', 'color', 'jaws', 'help']:
+    if section not in ['home', 'think', 'walk', 'attack', 'color', 'jaws']:
         section = 'home'
     BP.reset_all();
     return render_template('{}.html'.format(section), section=section)
