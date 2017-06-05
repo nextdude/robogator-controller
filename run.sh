ad42950c8f00d9f01358d3cea70579881374dfbc
Robert Lyons <nextdude@gmail.com>
2017-06-04 21:27:49 -0400

    updates

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-export FLASK_APP=app.py
-export FLASK_DEBUG=1
-FLASK_HOST=$(./get_ip.sh)
+export FLASK_HOST=$(./get_ip.sh)
 cd src
-flask run --host=$FLASK_HOST
+echo "Open http://${FLASK_HOST}:5000 in your browser"
+python3 app.py
