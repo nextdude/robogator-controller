c715386310ee108fdb752104c24c49514db4bfee
Robert Lyons <nextdude@gmail.com>
2017-06-04 22:38:42 -0400

    updates

--- src/app.py
+++ src/app.py
@@ -15,23 +15,44 @@ import json
 BP = brickpi3.BrickPi3()
 app = Flask(__name__)
 sockets = Sockets(app)
-active_scenario = None
+
+def init_scenario(scenario):
+    err = None
+    return err
+
+def handle_scenario_action(req):
+    res = { "status": "ok" }
+    if req['action'] == 'stop':
+        BP.reset_all()
+        res['status'] = 'scenario-done'
+    if req['action'] == 'start':
+        err = init_scenario(req['scenario'])
+        if err is not None:
+            res.status = "error"
+            res.error = err
+        else:
+            res.status = 'scenario-ready'
+    return res
 
 @app.route("/")
 def index():
     return show_section('home')
 
+
 @sockets.route("/gator")
 def gator(ws):
     while True:
-        msg = ws.receive()
-        ws.send(json.dumps(msg))
+        req = json.loads(ws.receive())
+        res = handle_scenario_action(req)
+        ws.send(json.dumps(res))
+
 
 @app.route("/<section>")
 def show_section(section):
-    if section not in ['home','think','walk','attack','color','jaws','help']:
+    if section not in ['home', 'think', 'walk', 'attack', 'color', 'jaws', 'help']:
         section = 'home'
     return render_template('{}.html'.format(section), section=section)
+
 
 if __name__ == "__main__":
     from gevent import pywsgi
---
+++ src/static/app.js
@@ -0,0 +1,53 @@
+
+(function() {
+  var socket, scenario, nextScenario;
+
+  function action(id) {
+    return JSON.stringify({scenario: scenario, action: id});
+  }
+
+  function onMessage(evt) {
+    var data = JSON.parse(evt.data);
+    console.log(data);
+    switch (data.status) {
+      case 'scenario-ready':
+        break;
+      case 'scenario-done':
+        if (nextScenario) {
+          location.replace("/"+nextScenario);
+        }
+        break;
+    }
+  }
+
+  function onOpen(evt) {
+    socket.send(action("start"))
+  }
+
+  function changeScenario(sc) {
+    nextScenario = sc;
+    socket.send(action("stop"));
+  }
+
+  function openConnection() {
+    socket = new WebSocket(location.protocol + "//" + location.host + "/gator");
+    socket.onopen = onOpen;
+    socket.onmessage = onMessage;
+  }
+
+  function onActionButtonClick(evt) {
+    socket.send(action(evt.target.id));
+  }
+
+  function init() {
+    actions = getElementsByClassName('kgc-action-button');
+    for (var i = 0; i < actions.length; i++)
+      actions[i].onclick = onActionButtonClick;
+    if (actions.length > 0) {
+      scenario = actions[0].id.replace(/-.*$/,'');
+      openConnection();
+    }
+  }
+
+  init();
+})();
